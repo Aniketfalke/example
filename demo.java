ddac55e348bc5eeab566f9d02246992181b70626
Aniket <falkeanya@gmail.com>
2022-04-28 00:14:50 +0530

    first commit

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo{
     public static void main(String[] args) {
-      System.out.println("hello everyone");
+      System.out.println("hello everyone ! welcome to git tutorial");
     }
 }
